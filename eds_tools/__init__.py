53d61192ef2cb788beb8d262c56ecd146602191c
ryanpdx <rmedick@pdx.edu>
2022-01-28 10:06:06 -0800

    add setup script

--- eds_tools/__init__.py
+++ eds_tools/__init__.py
@@ -1,11 +1,1 @@
-MAJOR = 0
-MINOR = 1
-PATCH = 0
-
-APP_NAME = 'eds-tools'
-APP_DESCRIPTION = 'A bunch of common EDS tools.'
-APP_VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
-APP_AUTHOR = 'Ryan Medick'
-APP_EMAIL = ''
-APP_URL = 'https://github.com/ryanpdx/eds-tools'
-APP_LICENSE = 'GPL-3.0'
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+import eds_tools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='eds-tools',
+    version=eds_tools.__version__,
+    license='GPL-3.0',
+    description='tools for editing CANopen EDS/DCF files',
+    long_description=long_description,
+    author='ryanpdx',
+    author_email='tbd',
+    maintainer='ryanpdx',
+    maintainer_email='tbd',
+    url='https://github.com/ryanpdx/eds-tools',
+    packages=['eds_tools'],
+    install_requires=[
+        'PyGObject',
+    ],
+    entry_points={
+        'console_scripts': [
+            'eds2dcf = eds_tools.eds2dcf:eds2dcf',
+            'eds-editor = eds_tools.eds_editor:eds_editor',
+            'validate-eds = eds_tools.validate_eds:validate_eds',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Embedded Systems',
+    ],
+)
