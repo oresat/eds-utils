cfc024abcd593840d1d2ead7f3e58bed2237250e
ryanpdx <rmedick@pdx.edu>
2022-07-20 21:36:38 -0700

    fix gui to work with no files as args

--- eds_tools/eds_editor/main.py
+++ eds_tools/eds_editor/main.py
@@ -12,9 +12,11 @@ def eds_editor(sys_args=None):
 
     name = 'eds-editor'
     parser = argparse.ArgumentParser(description=EDS_EDITOR_DESCRIPTION, prog=name)
-    parser.add_argument('filepath', metavar='FILEPATH', help='filepath to EDS file')
+    parser.add_argument('filepath', nargs='*', default='', metavar='FILEPATH',
+                        help='filepath to EDS file')
     args = parser.parse_args(sys_args)
 
     app = App()
-    app.open_file(args.filepath)
+    if args.filepath:
+        app.open_file(args.filepath[0])  # TODO handle multiple files
     app.run()
